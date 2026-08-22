b6d007064212249ed82ad06e790e3a74cff5e49a
Srishti Swaminathan <srishti.swaminathan@yale.edu>
2026-08-21 20:37:09 -0400

    Add Holly and Leo board photos, reorder board members
    - Add headshots for Holly Tran and Leo Chung (cropped/rotated to
      match existing team photo formatting)
    - Reorder board array to Sean, Srishti, Emma, Holly, Nadia, Leo, Usman

--- src/data/team.ts
+++ src/data/team.ts
@@ -22,6 +22,10 @@ import seanImg from '/sean.jpg'
 import emmaImg from '/emma.jpeg'
 // @ts-ignore
 import srishtiImg from '/srishti.png'
+// @ts-ignore
+import hollyImg from '/holly.jpg'
+// @ts-ignore
+import leoImg from '/leo.jpg'
 
 export interface TeamMember {
   name: string
@@ -39,13 +43,13 @@ export const leadership: TeamMember[] = [
 ]
 
 export const board: TeamMember[] = [
-  { name: 'Nadia Bakhtari', role: 'Co-Director of Outreach' },
-  { name: 'Leo Chung', role: 'Co-Director of Outreach' },
-  { name: 'Emma Lee', role: 'Co-Director of Membership', image: emmaImg },
-  { name: 'Holly Tran', role: 'Co-Director of Membership' },
-  { name: 'Usman Khalid', role: 'Director of Finance' },
-  { name: 'Srishti Swaminathan', role: 'Director of Communications', image: srishtiImg },
   { name: 'Sean Yu', role: 'Director of Research', image: seanImg },
+  { name: 'Srishti Swaminathan', role: 'Director of Communications', image: srishtiImg },
+  { name: 'Emma Lee', role: 'Co-Director of Membership', image: emmaImg },
+  { name: 'Holly Tran', role: 'Co-Director of Membership', image: hollyImg },
+  { name: 'Nadia Bakhtari', role: 'Co-Director of Outreach' },
+  { name: 'Leo Chung', role: 'Co-Director of Outreach', image: leoImg },
+  { name: 'Usman Khalid', role: 'Director of Finance' },
 ]
 
 export const advisors: TeamMember[] = [
